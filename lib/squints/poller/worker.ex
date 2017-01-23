38147e9b999f351e9a007d4973b0bd7282680431
Andrew Kraut <akraut@gmail.com>
2017-01-22 19:48:50 -0800

    Add logging for when the format is unknown

--- lib/squints/poller/worker.ex
+++ lib/squints/poller/worker.ex
@@ -139,8 +139,10 @@ defmodule Squints.Poller.Worker do
       |> Repo.insert_or_update
 
     Logger.debug(coord)
+    Logger.debug("Ins/Upd: #{result}")
   end
   defp store_coordinates(coord) do
+    Logger.error("Coordinate not in known format.")
     Logger.debug(Poison.encode!(coord))
   end
 
